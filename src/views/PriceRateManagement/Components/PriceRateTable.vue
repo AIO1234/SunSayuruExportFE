5750146f02aa5d6d87b1a578e79926012bee44f8
Yasindu <yasindu@sp-solutions.biz>
2024-02-04 23:01:03 +0530

    updated price rate view

--- src/views/PriceRateManagement/Components/PriceRateTable.vue
+++ src/views/PriceRateManagement/Components/PriceRateTable.vue
@@ -1,27 +1,42 @@
 <template>
-  <b-table sticky-header="" responsive="sm" :items="items" :fields="fields">
-    <template #cell(action)="data">
-      <b-button variant="none">
-        <b-img width="17px" src="@/assets/images/icons/Group 117855.png"></b-img>
-      </b-button>
-    </template>
-    <template #cell(status)="data">
-      <b-badge
-        v-if="data.value === 'Ongoing'"
-        style="background-color: #cdf59b; color: #67b108"
-        >{{ data.value }}</b-badge
-      >
-      <b-badge
-        v-else-if="data.value === 'Done'"
-        style="background-color: #ff8787; color: #de0000"
-        >{{ data.value }}</b-badge
-      >
-    </template>
-  </b-table>
+  <div>
+    <b-table sticky-header="" responsive="sm" :items="items" :fields="fields">
+      <template #cell(action)="data">
+        <b-button variant="none" @click="openDetailsModal(data.item)">
+          <b-img
+            width="17px"
+            src="@/assets/images/icons/Group 117855.png"
+          ></b-img>
+        </b-button>
+      </template>
+      <template #cell(status)="data">
+        <b-badge
+          v-if="data.value === 'Ongoing'"
+          style="background-color: #cdf59b; color: #67b108"
+          >{{ data.value }}</b-badge
+        >
+        <b-badge
+          v-else-if="data.value === 'Done'"
+          style="background-color: #ff8787; color: #de0000"
+          >{{ data.value }}</b-badge
+        >
+      </template>
+    </b-table>
+
+    <b-modal
+      ref="DetailsModal"
+      :title="`View ${selectedItem.suplier}'s Price Rates`"
+      title-class="modal_title_color"
+      hide-footer
+      size="lg"
+    >
+      <ViewPrices :selectedItem="selectedItem" />
+    </b-modal>
+  </div>
 </template>
 
 <script>
-
+import ViewPrices from "@/views/PriceRateManagement/Components/ViewRates.vue";
 import {
   BModal,
   BCard,
@@ -40,6 +55,7 @@ export default {
   name: "PriceRateTable",
   components: {
     BCard,
+    ViewPrices,
     BModal,
     BImg,
     BButton,
@@ -92,6 +108,10 @@ export default {
     setCellPadding(value, key, item) {
       // Add a custom class to table cells based on your requirements
       return "custom-cell-padding";
+    },
+    openDetailsModal(data) {
+      this.$refs.DetailsModal.show();
+      this.selectedItem = data;
     },
   },
 };
---
+++ src/views/PriceRateManagement/Components/ViewRates.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <br>
+    <b-table sticky-header="" responsive="sm" :items="items" :fields="fields">
+      <template #cell(action)="data">
+        <b-row no-gutters>
+          <b-col lg="4">
+            <b-button variant="none" @click="openUpdateModal(data.item)">
+              <b-img
+                width="17px"
+                src="@/assets/images/icons/Group 101.png"
+              ></b-img>
+            </b-button>
+          </b-col>
+          <b-col lg="4">
+            <b-button variant="none">
+              <b-img
+                width="17px"
+                src="@/assets/images/icons/Group 59.png"
+              ></b-img>
+            </b-button>
+          </b-col>
+        </b-row>
+      </template>
+    </b-table>
+  </div>
+</template>
+<script>
+import {
+  BModal,
+  BCard,
+  BTable,
+  BBadge,
+  BButton,
+  BCol,
+  BRow,
+  BImg,
+  BCardText,
+  BAvatar,
+  BLink,
+  BContainer,
+} from "bootstrap-vue";
+export default {
+  data() {
+    return {
+      fields: [
+        {
+          key: "seafoodtype",
+          label: "Seafood type",
+          sortable: true,
+
+          // tdClass: "custom-cell-padding",
+        },
+
+        {
+          key: "grading",
+          label: "Grading (Kg)",
+          sortable: true,
+          // thStyle: { width: "2%" },
+          // tdClass: "custom-cell-padding",
+        },
+
+        {
+          key: "quality",
+          label: "Quality",
+          sortable: true,
+          // thStyle: { width: "2%" },
+          // tdClass: "custom-cell-padding",
+        },
+
+        {
+          key: "pricerate",
+          label: "Price rate($)",
+          sortable: true,
+          // thStyle: { width: "2%" },
+          // tdClass: "custom-cell-padding",
+        },
+
+        {
+          key: "action",
+          label: "Action",
+          sortable: true,
+          // thStyle: { width: "2%" },
+          // tdClass: "custom-cell-padding",
+        },
+      ],
+      items: [
+        {
+          seafoodtype: "Prawns",
+          grading: "50 - 60",
+          quality: "A+",
+          pricerate: "100",
+        },
+        {
+          seafoodtype: "Prawns",
+          grading: "50 - 60",
+          quality: "A+",
+          pricerate: "100",
+        },
+        {
+          seafoodtype: "Prawns",
+          grading: "50 - 60",
+          quality: "A+",
+          pricerate: "100",
+        },
+      ],
+    };
+  },
+  components: {
+    BCard,
+    BModal,
+    BImg,
+    BButton,
+    BCol,
+    BBadge,
+    BAvatar,
+    BTable,
+    BRow,
+    BContainer,
+    BCardText,
+    BLink,
+  },
+  props: {
+    selectedItem: Object,
+  },
+};
+</script>
