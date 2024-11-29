30f013b209098eee32904782f8a508cd57e1ba6e
Sheyan Ariyasinghe <sheha.rashod.ariyasinghe@gmail.com>
2024-11-29 20:25:00 +0530

    update dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -23,12 +23,16 @@
             <span class="stats_title">Incomes, Expenses & Profit Analysis</span>
           </b-col>
 
+         
           <!-- type selection -->
           <b-col lg="2" cols="12">
+           
             <!-- space only for mobile -->
+
             <div class="mobile_only_view">
               <div class="mt-2"></div>
             </div>
+          
             <v-select
               class="selection"
               v-model="type"
@@ -38,14 +42,21 @@
               @input="dashboardOverview()"
             >
             </v-select>
+         
           </b-col>
+
+        
+
+          
 
           <!-- year selection -->
           <b-col lg="2" cols="12" class="padding_year">
+            
             <!-- space only for mobile -->
             <div class="mobile_only_view">
               <div class="mt-1"></div>
             </div>
+            
             <v-select
               class="selection"
               v-model="year"
